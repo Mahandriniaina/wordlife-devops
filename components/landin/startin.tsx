e09f7652ecebbdc72cf44142b2f02bdca446bff6
Judicaël RANDRIAMPANALINDAHY <judicaelrandriampanalindahy@gmail.com>
2025-07-14 07:48:10 +0000

    Update file startin.tsx

--- components/landin/startin.tsx
+++ components/landin/startin.tsx
@@ -46,8 +46,8 @@ const SignIn = () => {
 
       {/* Titre */}
       <Text style={styles.title}>Bienvenue</Text>
-      <Text style={styles.subtitle}>Veuillez vous connecter pour continuer Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis dignissimos et, unde soluta, voluptatum officia perferendis animi doloremque adipisci qui nihil nesciunt porro necessitatibus id. Ducimus officia nesciunt tenetur veritatis.</Text>
-
+      <Text style={styles.subtitle}>
+        Veuillez vous connecter pour continuer
       {/* Formulaire d'email */}
       <TextInput
         placeholder="Email"
